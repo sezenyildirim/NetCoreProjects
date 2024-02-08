102fcd76c48cedf1a1f435987a4d99a258fbbcff
sezenyildirim <bestesyildirim@gmail.com>
2024-02-08 17:36:54 +0300

    project2

---
+++ WebAPI-MemoryCache/WebAPI-MemoryCache/WebAPI-MemoryCache/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace WebAPI_MemoryCache.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private readonly IMemoryCache _memoryCache;
+
+		public ValuesController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			//_memoryCache.Remove("names");//cache temizlemek için
+			List<string> names = new();
+			names = _memoryCache.Get<List<string>>("names");
+			if (names == null)
+			{
+				names = new(){
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+			};
+				Thread.Sleep(3000);
+				_memoryCache.Set("names", names);
+				_memoryCache.Set("names", names,TimeSpan.FromSeconds(5));//cache'ledikten 5 sn sonra veriyi cache'den siler
+			}
+			return Ok(names);
+
+		}
+	}
+}
